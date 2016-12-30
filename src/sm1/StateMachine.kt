d51c9f90b3cd41d9d0255fd52b2500a4cf7e70eb
Stefan Dangl <sd@mobiworx.de>
2016-12-30 15:18:51 +0100

    minimization 1

--- src/sm1/StateMachine.kt
+++ src/sm1/StateMachine.kt
@@ -15,134 +15,9 @@ import kotlin.coroutines.suspendCoroutine
  */
 @RestrictsSuspension
 class StateMachine<Alphabet> internal constructor() {
-
-    protected val queue = LinkedBlockingQueue<Alphabet>()
-    protected var inputClosed = false
-    protected var inFinalState = false
-
     fun getInputStub(): Alphabet = null as Alphabet
-
-    @Throws(InputClosedException::class)
-    suspend fun getInput(): Alphabet {
-        return suspendCoroutine<Alphabet> { c: Continuation<Alphabet> ->
-            fun checkException() : Boolean {
-                synchronized(this) {
-                    val exc = exception
-                    if (exc == null) {
-                        return true
-                    } else {
-                        c.resumeWithException(exc)
-                        //notifAll()
-                        return false
-                    }
-                }
-            }
-            // blocks until next input is available
-            if (checkException()) {
-                do {
-                    if (queue.isEmpty() && inputClosed){
-                        throw InputClosedException()
-                    }
-                    val taken = queue.poll(1, TimeUnit.SECONDS)
-                    if (checkException()) {
-                        if (taken!=null) {
-                            c.resume(taken)
-                            notifyIfFinished()
-                        }else{
-                            if (inputClosed){
-                                throw InputClosedException()
-                            }
-                        }
-                    }
-                }while(taken==null)
-            }
-        }
-    }
-
-    var exception: Throwable? = null
-        private set
-
-    fun final() {
-        synchronized(this){
-            inFinalState = true
-            notifyIfFinished()
-        }
-    }
-    fun unfinal() {
-        synchronized(this) {
-            inFinalState = false
-        }
-    }
-
-    internal fun exception( exc: Throwable ) {
-        synchronized(this){
-            exception = exc
-            notifyIfFinished()
-        }
-    }
-
-    fun notifyIfFinished() {
-        synchronized(this) {
-            if (hasFinished())
-                (this as Object).notifyAll()
-        }
-    }
-
-    /** could potentially block, but should not **/
-    @Throws(InputClosedException::class)
-    fun input(i:Alphabet) {
-        if (exception!=null)
-            throw Exception(exception)
-        if (inputClosed){
-            throw InputClosedException()
-        }
-        queue.put(i)
-        if (slowFeeder>0){
-            val slow = (slowFeeder*(queue.size-1)).toLong()
-            if (slow>0) {
-                println("slowing feeder by $slow")
-                Thread.sleep(slow)
-            }
-        }
-    }
-
-    fun endInput() {
-        synchronized(this) {
-            inputClosed = true
-            notifyIfFinished()
-        }
-    }
-
-    /**
-     * returns true if final state was reached.
-     * returns false if timeout was reached.
-     * throws an exception if an error occurred.
-     */
-    fun waitForFinish(timeout: Long? = null) : Boolean {
-        if (exception!=null)
-            throw Exception(exception)
-        synchronized(this){
-            if (timeout!=null) {
-                (this as Object).wait(timeout)
-            }else{
-                while (!hasFinished()) {
-                    (this as Object).wait()
-                }
-            }
-        }
-        if (exception!=null)
-            throw Exception(exception)
-        return inFinalState
-    }
-
-    private fun hasFinished() = exception!=null || ( inFinalState && inputClosed && queue.isEmpty() )
-
-    /** slow down feeding thread if queue becomes big by pausing his thread **/
-    var slowFeeder: Double = 0.0
-
 }
 
-class InputClosedException : RuntimeException()
 
 fun <T> stateMachine(block: suspend StateMachine<T>.() -> Unit): StateMachine<T> {
 
@@ -151,15 +26,9 @@ fun <T> stateMachine(block: suspend StateMachine<T>.() -> Unit): StateMachine<T>
     Thread(){
         block.startCoroutine(stateMachine, object : Continuation<Unit> {
             override fun resume(value: Unit) {
-                //stateMachine.final()
             }
 
             override fun resumeWithException(exception: Throwable) {
-                if (exception is InputClosedException) {
-                    println("Input was closed in non-final state!")
-                } else {
-                    stateMachine.exception(exception)
-                }
             }
         })
     }.start()
--- src/sm1/StateMachineBuilder.kt
+++ src/sm1/StateMachineBuilder.kt
@@ -4,141 +4,15 @@ package sm1
  * Created by sd on 30.12.16.
  */
 
-@DslMarker
-annotation class StateMachineDSL
-
-@StateMachineDSL
-open class State<Alphabet> internal constructor(
-    var label: String? = null,
-    protected val body: State<Alphabet>.() -> Unit
-) {
-    var final: Boolean = false
-    internal fun visit() {
-        this.body()
-    }
-    internal val simpleTransitions = mutableMapOf<Alphabet, State<Alphabet>>()
-    internal val classBasedTransitions : MutableMap<Class<out Alphabet>, State<Alphabet>> = mutableMapOf()
-
-    internal var errorCallback: ((Alphabet) -> Unit)? = null
-    fun onError(function : (Alphabet) -> Unit) {
-        errorCallback = function
-    }/*
-    @Deprecated("loops should be defined outside of states")
-    fun loop(letter:T){}
-    @Deprecated("simpleTransitions should be defined outside of states")
-    fun via(letter: T, b:Any?){}*/
-
-    override fun toString(): String = label?:"State ${hashCode()}"
-    val noTransitions: Boolean
-      get() = simpleTransitions.isEmpty() && classBasedTransitions.isEmpty()
-}
-/*
-class Transition<T> internal constructor(
-    val builder : StateMachineBuilder<T>,
-    val fromState: State<T>, val letter: T, val nextState: State<T>
-) {/*
-    fun loop( letter: T ) = builder.transition(nextState,letter,nextState)
-    fun via(letter: T, nextState: State<T> ) = builder.transition(this.nextState,letter,nextState)
-    fun via(letter: T, nextState: State<T>.() -> Unit ) = builder.transition(this.nextState, letter, nextState)*/
-}*/
-
-internal class FakeInitialState<Alphabet> : State<Alphabet>( "Initial State", {} ) {
-    var used = false
-}
-
-@StateMachineDSL
-class StateMachineBuilder<Alphabet> internal constructor(val block: StateMachineBuilder<Alphabet>.() -> Unit){
-
-    internal val fakeInitialState = FakeInitialState<Alphabet>()
-    var initialState : State<Alphabet> = fakeInitialState
-    var currentState: State<Alphabet> = fakeInitialState
-
-    protected fun use(state: State<Alphabet>) {
-        if (state==fakeInitialState){
-            fakeInitialState.used=true
-        }
-    }
-
-    fun state( label:String?=null, function: State<Alphabet>.() -> Unit = {} ): State<Alphabet> {
-        val state = State<Alphabet>(label,function)
-        if (initialState===fakeInitialState && !fakeInitialState.used){
-            initialState = state
-        }
-        currentState = state
-        return state
-    }
-    fun state( function: State<Alphabet>.() -> Unit = {} ): State<Alphabet> = state(null,function)
-
-    fun transition(fromState: State<Alphabet>, letter: Alphabet, nextState: State<Alphabet>): State<Alphabet> {
-        fromState.simpleTransitions[letter] = nextState
-        use(fromState)
-        return nextState //Transition(this,fromState,letter,nextState)
-    }
-    fun transition(fromState: State<Alphabet>.() -> Unit, letter: Alphabet, toState: State<Alphabet>.() -> Unit) =
-        transition(state(fromState), letter, state(toState))
-    fun transition(fromState: State<Alphabet>, letter: Alphabet, toState: State<Alphabet>.() -> Unit): State<Alphabet> {
-        use(fromState)
-        return transition(fromState,letter,state(toState))
-    }
-
-    fun transition(fromState: State<Alphabet>, clazz: Class<out Alphabet>, nextState: State<Alphabet>): State<Alphabet> {
-        fromState.classBasedTransitions[clazz] = nextState
-        use(fromState)
-        return nextState //Transition(this,fromState,letter,nextState)
-    }
-    fun transition(fromState: State<Alphabet>.() -> Unit, clazz: Class<out Alphabet>, toState: State<Alphabet>.() -> Unit) =
-            transition(state(fromState), clazz, state(toState))
-    fun transition(fromState: State<Alphabet>, clazz: Class<out Alphabet>, toState: State<Alphabet>.() -> Unit): State<Alphabet> {
-        use(fromState)
-        return transition(fromState,clazz,state(toState))
-    }
-
-    fun via(letter: Alphabet, nextState: State<Alphabet> ) = transition(currentState, letter, nextState)
-    fun via(letter: Alphabet, nextState: State<Alphabet>.() -> Unit ) = transition(currentState, letter, nextState)
-    fun via(clazz: Class<out Alphabet>, nextState: State<Alphabet> ) = transition(currentState, clazz, nextState)
-    fun via(clazz: Class<out Alphabet>, nextState: State<Alphabet>.() -> Unit ) = transition(currentState, clazz, nextState)
-
-    fun loop(letter: Alphabet) = transition(currentState,letter, currentState)
-    fun loop(clazz: Class<out Alphabet>) = transition(currentState,clazz, currentState)
+class StateMachineBuilder<Alphabet> internal constructor(){
 
     fun createStateMachine(): StateMachine<Alphabet> = stateMachine {
-        this@StateMachineBuilder.block()
-        var state: State<Alphabet> = initialState
-        state.visit()
-        while (!state.final || !state.noTransitions){
-            val letter = getInputStub()
-            val nextState : State<Alphabet>? =
-                state.simpleTransitions[letter] ?:
-                if (letter is Object)
-                    state.classBasedTransitions[letter.javaClass as Class<out Alphabet>]
-                else null
-            if (nextState!=null){
-                unfinal()
-                state = nextState
-                state.visit()
-                if (state.final){
-                    final()
-                }
-            } else {
-                state.errorCallback?.invoke(letter)
-                errorCallback?.invoke(state, letter)
-            }
-        }
+        val letter = getInputStub()
+        val nextState : Int =
+            if (letter is Object)
+                1
+            else
+                2
     }
 
-    private var errorCallback: ((State<Alphabet>, letter: Alphabet) -> Unit)? = null
-
-    fun onError(function: (State<Alphabet>, letter: Alphabet) -> Unit) {
-        errorCallback = function
-    }
-
-    var label: String?
-        set(value) { currentState.label = value }
-        get() = currentState.label
-
-}
-
-fun <T> buildStateMachine(block: StateMachineBuilder<T>.() -> Unit): StateMachine<T> {
-    val builder = StateMachineBuilder<T>(block)
-    return builder.createStateMachine()
 }
